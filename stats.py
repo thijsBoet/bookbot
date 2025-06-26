84794a42351dbf41bab462e0dd51247ce5004316
Matthijs Boet <m.boet@chello.nl>
2025-06-26 16:25:42 +0200

    char dict made

---
+++ stats.py
@@ -0,0 +1,13 @@
+def get_num_words(text):
+    num_words = len(text.split())
+    return num_words
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
